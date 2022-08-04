35dc65d5e1fc6cc905cca0eb05b9a5cd35481269
Yann <metalyanni@gmail.com>
2022-08-04 18:54:39 -0500

    Agregando dinamismo a heroes template

--- Seccion3 - IntroAngular/01-bases/src/app/heroes/heroe/heroe.component.ts
+++ Seccion3 - IntroAngular/01-bases/src/app/heroes/heroe/heroe.component.ts
@@ -8,7 +8,7 @@ export class HeroeComponent{
     
     nombre: string = 'Iron Man'
     edad  : number = 45;
-
+    pic: string = "https://66.media.tumblr.com/97279c37264ccdf40302a12ff33958b3/tumblr_mgrbnb2nYr1rfjowdo1_r1_500.gif";
     /** Al llamarse sólo se pone como nombreCapitalizado */
     get nombreCapitalizado(): string{
         return this.nombre.toUpperCase();
@@ -21,9 +21,12 @@ export class HeroeComponent{
 
     cambiarNombre(): void{
         this.nombre = 'Spiderman';
+        this.pic = "https://64.media.tumblr.com/tumblr_mavnqdzzI81qeh39oo1_r2_500.gifv";
     }
 
     cambiarEdad(): void{
         this.edad = 30;
     }
+
+
 }
