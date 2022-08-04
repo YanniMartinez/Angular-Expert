14fc31a920143fac671fb8fef4b7e85b90296044
Yann <metalyanni@gmail.com>
2022-08-04 11:10:14 -0500

    Diferencia entre clases e interfaces

--- Seccion2 - TypeScript/01-ts-intro/src/index.ts
+++ Seccion2 - TypeScript/01-ts-intro/src/index.ts
@@ -1,3 +1,38 @@
 /*
     ===== Código de TypeScript =====
 */
+
+/**
+ * Las interfaces no existen en JS, pero las clases si, por lo
+ * que esas si podremos verlas sin ningun problema
+ * aun que son muy semejantes tiene claras diferencias de uso:
+ * 
+ * La interfaz no define como funcionan los metodos, constructores, gets o sets
+ * Las clases permiten instanciar
+ */
+
+class Heroe{
+    alterEgo: string;
+    edad: number;
+    nombreReal: number;
+
+    //Aquí si podemos decir que hace y como lo hace
+    imprimirNombre(){
+        return this.alterEgo + ' ' + this.nombreReal;
+    }
+}
+
+interface Personaje3{
+    alterEgo: string;
+    edad: number;
+    nombreReal: number;
+
+    //Aquí solo definimos tipos de datos o esqueleto
+    imprimirNombre: () => string;
+}
+
+const ironman = new Heroe();
+
+//const spiderman: Personaje3 = {};
+
+console.log(ironman);
