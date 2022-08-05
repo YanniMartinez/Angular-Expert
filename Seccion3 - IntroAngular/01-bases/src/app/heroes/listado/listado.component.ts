267bb8b2e570e4767b3c0ceade2612f427d338d0
Yann <metalyanni@gmail.com>
2022-08-04 21:01:32 -0500

    *ngfor - Nuestro FOR para ejecutarse de parte del component.html

--- Seccion3 - IntroAngular/01-bases/src/app/heroes/listado/listado.component.ts
+++ Seccion3 - IntroAngular/01-bases/src/app/heroes/listado/listado.component.ts
@@ -6,6 +6,16 @@ import { Component} from '@angular/core';
   styleUrls: ['./listado.component.css']
 })
 export class ListadoComponent {
-
-
+  
+  heroeBorrado: string = '';
+  heroes: string[] = ['Spiderman','Iron Man', 'Hulk','Thor','Capitan America'];
+  
+  /**Permite borrar el ultimo elemento del arreglo
+   * este método será llamada por el listado.component el cual 
+   * renderizará el contenido en la página
+   */
+  borrarHeroe(){
+    //Si no puede borrar nada entonces que retorne cadana vacia
+    this.heroeBorrado = this.heroes.pop() || '';
+  }
 }
