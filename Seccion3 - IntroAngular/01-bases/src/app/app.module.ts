d67c84894c3759d7d7dd7dad0af3c279a39cffbc
Yann <metalyanni@gmail.com>
2022-08-05 13:46:51 -0500

    Modulos pt2- Modularización de todas las funcionalidades

--- Seccion3 - IntroAngular/01-bases/src/app/app.module.ts
+++ Seccion3 - IntroAngular/01-bases/src/app/app.module.ts
@@ -2,18 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador/contador.component';
+import { ContadorModule } from './contador/contador.module';
 import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
   declarations: [
     //Se deben importar todos los modulos a usar
-    AppComponent, 
-    ContadorComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    HeroesModule //Importamos el modulo recien creado
+    ContadorModule,
+    HeroesModule, //Importamos el modulo recien creado
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Seccion3 - IntroAngular/01-bases/src/app/heroes/heroes.module.ts
+++ Seccion3 - IntroAngular/01-bases/src/app/heroes/heroes.module.ts
@@ -14,6 +14,7 @@ import { ListadoComponent } from "./listado/listado.component";
         ListadoComponent
     ],
     imports: [
+        //El CommonModule importa algunas directivas como los ngIf, ngFor
         CommonModule
     ]
 })
